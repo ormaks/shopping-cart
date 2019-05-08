68b4dbe8ed0219135b6606a9315d2f41c4f99e6b
Max Chutailo <m.chutailo@benamix.solutions>
2019-05-08 13:41:52 +0300

    Add bin, add sign up with passport, adding products to bin, add profile page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product')
+var Product = require('../models/product');
+var Cart = require('../models/cart');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Product.find(function (err, docs) {
-        var productChunk=[];
+        var productChunk = [];
         var chunkSize = 3;
-        for(var i=0;i<docs.length;i+=chunkSize){
-            productChunk.push(docs.slice(i,i +chunkSize))
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            productChunk.push(docs.slice(i, i + chunkSize))
         }
         res.render('shop/index', {title: 'Shopping cart', products: productChunk});
     });
 });
+
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+    Product.findById(productId, function (err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    })
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
 
 module.exports = router;
